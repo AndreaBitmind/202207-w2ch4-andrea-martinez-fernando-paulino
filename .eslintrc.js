fd199b99a31940cbdf73a3f616c6f5a7573e1c8e
Andrea Martinez <andrea.maort@gmail.com>
2022-07-14 22:36:59 +0200

    test completed succesfully

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,9 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
+    jest: true,
   },
-  extends: ["airbnb-base", "prettier"],
+  extends: ["airbnb-base", "prettier", "jest"],
   parserOptions: {
     ecmaVersion: "latest",
   },
